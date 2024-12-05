5d7cb3dc3b2c9f6f1989595017287d50074e38f1
Yova de León <erwingeo1021@gmail.com>
2024-12-04 20:40:24 -0600

    :sparkles: Se agrega ejemplos de la sesión #4

---
+++ sesion_4/Ejemplos/Ejemplo1/Estudiante.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejemplo1
+{
+    class Estudiante
+    {
+        int codEstudiante;
+        string nombres;
+        string apellidos;
+        string grado;
+        string seccion;
+        DateTime fecNacimiento;
+        string noCarne;
+
+        public Estudiante() { 
+            codEstudiante = 0;
+            nombres = "Jhon";
+            apellidos = "Doe";
+            grado = "Primaria";
+            seccion = "B";
+        }
+
+        public Estudiante(int codEstudiante, string nombres, string apellidos)
+        {
+            this.codEstudiante = codEstudiante;
+            this.nombres = nombres;
+            this.apellidos = apellidos;
+            grado = "Primaria";
+            seccion = "B";
+        }
+
+        public Estudiante(int codEstudiante, string nombres, string apellidos,
+            string grado, string seccion, DateTime fecNacimiento, string noCarne) { 
+
+            this.codEstudiante = codEstudiante;
+            this.nombres = nombres;
+            this.noCarne = noCarne;
+            this.apellidos = apellidos;
+            this.grado = grado;
+            this.seccion = seccion;
+            this.fecNacimiento = fecNacimiento;
+        }
+
+        // Método que permite obtener los nombres y apellidos del estudiante
+        public string obtNombreCompleto() {
+            return $"{nombres} {apellidos}";
+        }
+
+        public void mostrarInformacionEstudiante()
+        {
+            Console.WriteLine($"COD Estudiante: {codEstudiante}");
+            Console.WriteLine($"Nombre Completo: {nombres} {apellidos}");
+            Console.WriteLine($"No. Carné: {noCarne}");
+            Console.WriteLine($"Grado: {grado} - Sección: {seccion}");
+            Console.WriteLine($"Edad del Estudiante: {obtEdad()}");
+        }
+
+        public int obtEdad() {
+            DateTime fecHoy = DateTime.Today;
+
+            int edad = fecHoy.Year - fecNacimiento.Year;
+
+            return edad;
+        } 
+    }
+}
---
+++ sesion_4/Ejemplos/Ejemplo1/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace Ejemplo1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Estudiante estudiante1 = new Estudiante();
+
+            estudiante1.mostrarInformacionEstudiante();
+
+            Estudiante estudiante2 = new Estudiante(2,
+                "Leonel",
+                "Ramos",
+                "Graduado",
+                "N/A",
+                new DateTime(1999, 02, 03),
+                "110002133");
+
+            estudiante2.mostrarInformacionEstudiante();
+
+            Estudiante estudiante3 = new Estudiante(3,"Pedro","Ibañez");
+
+            estudiante3.mostrarInformacionEstudiante();
+
+
+
+        }
+    }
+}
